f9d0126bd0deef0cf76ce7c7d61d09c680036faf
parsa <parsa2017roohi@gmail.com>
2021-08-03 20:09:08 +0430

    asp.net core signalR

---
+++ SignalR/Hubs/chatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class chatHub : Hub
+    {
+        public void send(string name, string message)
+        {
+            //Clients.Client("connectionString").send();
+            Clients.All.addNewMessage(name, message);
+        }
+    }
+}
